e067b0a94a0cb2d30609ab913fbc2fbc4f15c47d
余颖 <yy28@jacksoft.cn>
2016-01-25 22:54:13 +0800

    改为API测试代码

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM node:5.4
+
+EXPOSE 80
+
+CMD [ "node","index.js"]
--- index.js
+++ index.js
@@ -1,12 +1,42 @@
-#!/usr/local/bin/node
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
-var server = require("./server");
-var router = require("./router");
-var requestHandlers = require("./requestHandlers");
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
 
-var handle = {}
-handle["/"] = requestHandlers.start;
-handle["/start"] = requestHandlers.start;
-handle["/upload"] = requestHandlers.upload;
+io.of('/api/io').on('connection', function(socket){
+	console.log('connection');
 
-server.start(router.route, handle);
+	// chat
+	socket.on('AuthLogin', function(msg){
+		console.log('AuthLogin: ' + msg);
+		var rtn = `
+	{
+	    "Token": "xxxxx", // Token令牌。有效期为15分钟。每次成功调用API，都会延期15分钟
+	}
+		`;
+		socket.emit('AuthLogin', rtn);
+	});
+
+	// news
+	socket.on('AuthLogout', function(msg){
+		console.log('AuthLogout: ' + msg);
+		var rtn = `
+	{
+	    // 无
+	}
+		`;
+		socket.emit('AuthLogout', rtn);
+	});
+
+
+
+});
+
+
+
+http.listen(80, function(){
+  console.log('listening on *:80');
+});
